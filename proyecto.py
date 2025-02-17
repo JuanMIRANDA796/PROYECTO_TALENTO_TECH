6dc0270c56126fe1391f9e7b0d7d8f652ca7f4aa
JuanMIRANDA796 <juan.mirandar@udea.edu.co>
2025-02-17 07:15:22 -0500

    Update proyecto.py

--- proyecto.py
+++ proyecto.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-import plotply.express as px
+import plotly.express as px
 from datetime import datetime
 
 #1. Configuración inical de la aplicacion
